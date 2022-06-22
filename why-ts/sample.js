63611ed653fe96134b18f60416a664b94af2a61f
BrianH <brianhwang29@gmail.com>
2022-06-22 13:08:16 +0900

    initial study

---
+++ why-ts/sample.js
@@ -0,0 +1,20 @@
+// function add(a , b){
+//   return a + b;
+// }
+
+// // js는 자동완성이 안됨.
+// const res = add(10,20);
+
+
+// @ts-check
+/**
+ * 
+ * @param {*} a 첫번째 숫자
+ * @param {*} b 두번째 숫자
+ * @returns 
+ */
+function add(a , b){
+  return a + b;
+}
+
+add(10,'20');
